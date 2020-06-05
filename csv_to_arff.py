226937ca540dedd6eb9d30d5f322d87a3818dd97
jal04 <josemi.bnf@outlook.es>
2020-06-05 20:18:58 +0200

    learn and evaluate

--- csv_to_arff.py
+++ csv_to_arff.py
@@ -1,8 +1,14 @@
 
 
-def to_arff(file):
+def divide(file):
     pass
+
+def to_arff(file):
+    for l in file.readlines():
+        print(l)
 
 if __name__ == "__main__":
     file = open("barca.csv","r")
-    arff = to_arff(file)
+    learn, evaluate = divide(file)
+    arff_learn = to_arff(learn)
+    arff_evaluate = to_arff(evaluate)
