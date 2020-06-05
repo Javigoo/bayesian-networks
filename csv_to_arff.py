43abee1082e66cfc7be084a08a26905ad145202d
jal04 <josemi.bnf@outlook.es>
2020-06-05 20:54:29 +0200

    using pandas

--- csv_to_arff.py
+++ csv_to_arff.py
@@ -1,14 +1,16 @@
-
+import csv
+import pandas
 
 def divide(file):
-    pass
+    df = pandas.read_csv(file)
+    print(df)
 
 def to_arff(file):
     for l in file.readlines():
         print(l)
 
 if __name__ == "__main__":
-    file = open("barca.csv","r")
+    file = "barca.csv"
     learn, evaluate = divide(file)
     arff_learn = to_arff(learn)
     arff_evaluate = to_arff(evaluate)
